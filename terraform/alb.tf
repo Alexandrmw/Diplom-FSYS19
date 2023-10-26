6efe41e0a23e110f21ff615804722398f9ceefa4
Alexandrmw <alexandrmw@gmail.com>
2023-10-26 21:04:58 +0300

    8

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,7 +1,7 @@
 #Таргет группа/Целевая группа
 
-resource "yandex_alb_target_group" "web" {
-  name = "web"
+resource "yandex_alb_target_group" "tg-web" {
+  name = "tg-web"
 
   target {
     subnet_id  = yandex_vpc_subnet.bastion-internal-segment.id
@@ -16,10 +16,10 @@ resource "yandex_alb_target_group" "web" {
 
 #Группа бэкэндов
 
-resource "yandex_alb_backend_group" "web-servers" {
+resource "yandex_alb_backend_group" "alb-bg" {
   http_backend {
-    name             = "web-servers"
-    target_group_ids = ["${yandex_alb_target_group.web.id}"]
+    name             = "alb-bg-1"
+    target_group_ids = ["${yandex_alb_target_group.tg-web.id}"]
     port             = 80
     healthcheck {
       timeout  = "10s"
@@ -39,15 +39,14 @@ resource "yandex_alb_http_router" "web-servers-router" {
   name = "web-servers-router"
 }
 
-resource "yandex_alb_virtual_host" "web-servers-router-virtual-host" {
-  name           = "web-servers-router-virtual-host"
+resource "yandex_alb_virtual_host" "alb-host" {
+  name           = "alb-host"
   http_router_id = yandex_alb_http_router.web-servers-router.id
   route {
     name = "my-route"
     http_route {
       http_route_action {
-        backend_group_id = yandex_alb_backend_group.web-servers.id
-#       backend_group_id = <идентификатор_группы_бэкендов>
+        backend_group_id = yandex_alb_backend_group.alb-bg.id
         timeout           = "60s"
       }
     }
@@ -57,23 +56,31 @@ resource "yandex_alb_virtual_host" "web-servers-router-virtual-host" {
 
 #L7-балансировщик
 
-resource "yandex_alb_load_balancer" "app-lb" {
-  name       = "app-lb"
+resource "yandex_alb_load_balancer" "alb-lb" {
+  name       = "alb-lb"
+
   network_id = yandex_vpc_network.bastion-network.id
+
 #  security_group_ids = [ yandex_vpc_security_group.external-ssh-sg.id, yandex_vpc_security_group.internal-ssh-sg.id ]
 #  security_group_ids = ["<идентификатор_группы_безопасности>"]
-security_group_ids = [ yandex_vpc_security_group.alb-sg.id, yandex_vpc_security_group.egress-sg.id, yandex_vpc_security_group.web-sg.id, yandex_vpc_security_group.external-ssh-sg.id, yandex_vpc_security_group.internal-ssh-sg.id   ]
+  security_group_ids = [ yandex_vpc_security_group.alb-sg.id,
+                         yandex_vpc_security_group.egress-sg.id,
+                         yandex_vpc_security_group.alb-vm-sg.id,
+                         yandex_vpc_security_group.external-ssh-sg.id,
+                         yandex_vpc_security_group.internal-ssh-sg.id
+                       ]
 
   allocation_policy {
     location {
       zone_id   = "ru-central1-a"
-      subnet_id = yandex_vpc_subnet.bastion-external-segment.id
+      subnet_id = yandex_vpc_subnet.bastion-external-segment.id  /* Уточнить в следующий раз на internal */
     }
   }
 
 
   listener { /* описание параметров обработчика для L7-балансировщика */
-    name = "listener-1"
+    name = "alb-listener"
+    
     endpoint {
       address {
         external_ipv4_address {
@@ -81,6 +88,7 @@ security_group_ids = [ yandex_vpc_security_group.alb-sg.id, yandex_vpc_security_
       }
       ports = [ 80 ]
     }
+
     http {
       handler {
         http_router_id = yandex_alb_http_router.web-servers-router.id /* <идентификатор_HTTP-роутера> */
--- terraform/host-bastion.tf
+++ terraform/host-bastion.tf
@@ -1,9 +1,10 @@
 # Бастион хост
 resource "yandex_compute_instance" "bastion-host" {
 
-  zone     = "ru-central1-a"
-  name     = "bastion"
+  name     = "bastion-host"
   hostname = "bastion"
+  zone     = "ru-central1-a"
+  allow_stopping_for_update = true
 
   resources {
     core_fraction = 20
@@ -20,14 +21,20 @@ resource "yandex_compute_instance" "bastion-host" {
 
   network_interface {
     subnet_id = yandex_vpc_subnet.bastion-external-segment.id
-    security_group_ids = [yandex_vpc_security_group.external-ssh-sg.id,
-    yandex_vpc_security_group.internal-ssh-sg.id]
+
+    security_group_ids = [
+                          yandex_vpc_security_group.external-ssh-sg.id,
+                          yandex_vpc_security_group.internal-ssh-sg.id,
+                          yandex_vpc_security_group.zabbix-sg.id,
+                          yandex_vpc_security_group.egress-sg.id
+                         ]
+
     nat        = true
     ip_address = "192.168.50.10"
   }
 
   metadata = {
-    user-data = "${file("./meta-bastion.yaml")}"
+    user-data = "${file("./meta.yaml")}"
   }
 
   scheduling_policy {
