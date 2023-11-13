e569e32595f06e1cd667d2d99d8b384bc2f5f5c1
Alexandrmw <alexandrmw@gmail.com>
2023-11-13 23:53:40 +0300

    bugs fix

--- terraform/snapshot.tf
+++ terraform/snapshot.tf
@@ -16,7 +16,7 @@ resource "yandex_compute_snapshot_schedule" "snapshot" {
   disk_ids = [
     "${yandex_compute_instance.bastion.boot_disk.0.disk_id}",
     "${yandex_compute_instance.webserver-1.boot_disk.0.disk_id}",
-    "${yandex_compute_instance.webserver-1.boot_disk.0.disk_id}",
+    "${yandex_compute_instance.webserver-2.boot_disk.0.disk_id}",
     "${yandex_compute_instance.zabbix-server.boot_disk.0.disk_id}",
     "${yandex_compute_instance.elasticsearch.boot_disk.0.disk_id}",
     "${yandex_compute_instance.kibana.boot_disk.0.disk_id}", ]
